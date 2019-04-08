46ff1163a573eb2668f94efc9ebec627de2ab6f0
Anthony McDonald <a@awews.com>
2019-04-08 13:17:53 -0700

    add more methods to find user by id and role

--- src/main/java/com/intraedge/project/prok/repositories/UserRepository.java
+++ src/main/java/com/intraedge/project/prok/repositories/UserRepository.java
@@ -2,8 +2,14 @@ package com.intraedge.project.prok.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.intraedge.project.prok.domain.User;
+import com.intraedge.project.prok.domains.User;
 
 public interface UserRepository  extends MongoRepository<User, String> {
 	User findUserBy_id(String _id);
+	User findUserByUsername(String username);
+	User findUserByUserRole(String userRole);
+	
+	User getUserBy_id(String _id);
+	User getUserByUsername(String username);
+	User getUserByUserRole(String userRole);
 }
